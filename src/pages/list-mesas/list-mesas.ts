fb5a0e151ad8117b9bfb8f5d3c8332daed510d27
Diógenes Rafael <diogerafael@gmail.com>
2018-01-11 00:33:53 -0200

    iniciaaanddo  aarr

--- src/pages/list-mesas/list-mesas.ts
+++ src/pages/list-mesas/list-mesas.ts
@@ -21,59 +21,30 @@ import {ListSecaoPage} from "../list-secao/list-secao";
 export class ListMesasPage {
   //  mesas: Array<{ numero: number, status: number }>;
    mesas:Mesa[];
-
-  //  [
-  //   {
-  //      dataFechamento:string,
-  //      codem:number,
-  //      horaFechamento:string,
-  //      codigo:number,
-  //      cer:number,
-  //      total:number,
-  //      codcai:number,
-  //      "coo":0,
-  //      "status":0,
-  //      "cooVenda":0,
-  //      "contaOrigem":0,
-  //      "dataAberura":"2017-12-28T00:00:00.000Z",
-  //      "conta":10,
-  //      "horaAberura":0.99846931712963,
-  //      "itens":[
-  //         {
-  //            "quant":1,
-  //            "valor":3,
-  //            "codEmp":1,
-  //            "cancelado":0,
-  //            "codigo":10,
-  //            "codPro":1,
-  //            "codVend":1,
-  //            "contaOrigem":0,
-  //            "ordem":1,
-  //            "nomePro":"PALHA ITALIANA"
-  //         }
-  //      ]
-  //   }
+  mesasAbertas:Mesa[];
 
    myColorClosed = "#199716";
    myColorOpened = "#ef1240";
 
   constructor(public navCtrl: NavController, public navParams: NavParams,
     private mesasServiceProvider:MesasServiceProvider ) {
-    // this.mesas = [{numero:1,
-    //   status:0},
-    //   {numero:2,
-    //   status:0},
-    //   {numero:3,
-    //   status:1},
-    //   {numero:4,
-    //   status:0}
-    // ];
+    // for (var i = 1; i < 11; i++) {
+    //   this.mesas.push(new Mesa(i));
+    // }
+
   }
 
   ionViewDidLoad() {
     //load mesas
-    this.mesasServiceProvider.getMesas().subscribe(Vendas =>
-      this.mesas = Vendas)
+    this.mesasServiceProvider.getMesas().subscribe(Vendas => {
+      this.mesasAbertas = Vendas;
+        if (this.mesas.length > 10){
+          console.log(this.mesas);
+        }},
+      (error: any)=>{
+        console.log(1);
+      });
+
   }
 
   openPageSecao(){
--- src/pages/list-mesas/mesa.model.ts
+++ src/pages/list-mesas/mesa.model.ts
@@ -1,16 +1,20 @@
 export interface Mesa {
-    dataFechamento:string
-    codem:number
-    horaFechamento:string
-    codigo:number
-    cer:number
-    total:number
-    codcai:number
-    coo:number
-    status:number
-    cooVenda:number
-    contaOrigem:number
-    dataAberura:string
-    conta:number  
-    horaAberura:string
+    dataFechamento:string;
+    codem:number;
+    horaFechamento:string;
+    codigo:number;
+    cer:number;
+    total:number;
+    codcai:number;
+    coo:number;
+    status:number;
+    cooVenda:number;
+    contaOrigem:number;
+    dataAberura:string;
+    conta:number;
+    horaAberura:string;
+
+    // constructor(Codigo:number){
+    //   this.codigo = Codigo;
+    // }
 }
